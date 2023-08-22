0576a0aeae038ab4b2a9755b4e1dc80279dd989f
Naftali <nalimaemurugu@gmail.com>
2023-08-22 10:07:09 +0300

    new features of the ecommerce shop

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 #the Cart class that will allow you to manage the shopping cart
 #First, you try to get the cart from the current session using self.session.get(settings.CART_SESSION_ID).
@@ -16,6 +17,25 @@ class Cart:
 			#save an empty cart in the session
 			cart = self.session[settings.CART_SESSION_ID]={}
 		self.cart = cart
+		#store current applied coupon
+		self.coupon_id = self.session.get('coupon_id')
+	@property
+	def coupon(self):
+		if self.coupon_id:
+			try:
+				return Coupon.objects.get(id=self.coupon_id)
+			except Coupon.DoesNotExist:
+				pass
+				return None
+
+	def get_discount(self):
+		if self.coupon:
+			return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+		return Decimal(0)
+		
+	def get_total_price_after_discount(self):
+		return self.get_total_price()- self.get_discount()
+		
 #method to add products to the cart or update their quantity.
 	def add(self, product, quantity=1, override_quantity=False):
 		"""
